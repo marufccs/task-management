c6cd041ca067359724556fe823f6855e0503a24d
unknown <marufmozumder7@gmail.com>
2023-01-03 19:17:44 +0100

    completed-tasks-done

---
+++ src/components/CompletedTasks/CompletedTask.js
@@ -0,0 +1,119 @@
+import { useQuery } from '@tanstack/react-query';
+import { Button } from 'flowbite-react';
+import React, { useContext, useState } from 'react';
+import Swal from 'sweetalert2';
+import { AuthContext } from '../../context/UserContext';
+
+const CompletedTask = ({completedtask}) => {
+
+    const {name, image, description} = completedtask;
+
+    const {user} = useContext(AuthContext);
+    const email = user.email;
+
+    const { data: completedtasks = [], isLoading, refetch} = useQuery({
+        queryKey: ['completedtasks'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/completedtasks?email=${user.email}`);
+            const data = await res.json();
+            return data
+        }
+    })
+
+    const [displayTasks, setDisplayTasks] = useState(completedtasks);
+
+    const handleDelete = _id => {
+        Swal.fire({
+            title: `Are you sure you want to delete ${name}?`,
+            text: "You won't be able to undo this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              fetch(`http://localhost:5000/completedtasks/${completedtask._id}`, {
+                method: 'DELETE'
+              })
+              .then(res => res.json())
+              .then(data => {
+                if(data.deletedCount > 0){
+                  Swal.fire(
+                    'Great!',
+                    'The task has been deleted successfully!',
+                    'success'
+                  )
+                  const remainingTasks = displayTasks.filter(task => task._id !== completedtask._id)
+                    setDisplayTasks(remainingTasks);
+                    refetch();
+                }
+              })
+            }
+          })
+         
+    }
+
+    const handleMarkAsIncomplete = _id => {
+        fetch(`http://localhost:5000/mytasks`, {
+            method: "POST",
+            headers: {
+              "content-type" : "application/json"
+            },
+            body: JSON.stringify({name, image, description, email})
+          })
+          .then(res => res.json())
+          .then(data => {
+            Swal.fire(
+              'Great!',
+              'The task has been marked as incomplete!',
+              'success'
+            )
+            if(data.acknowledged === true){
+                fetch(`http://localhost:5000/completedtasks/${completedtask._id}`, {
+               method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+          if(data.deletedCount > 0){          
+            const remainingTasks = displayTasks.filter(task => task._id !== completedtask._id)
+              setDisplayTasks(remainingTasks);
+              refetch();
+          }
+        })
+            }
+            })
+
+    }
+
+    return (
+        <div>
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+
+<img className="rounded-t-lg h-60 w-full" src={image} alt="" />
+<div className="p-5">
+<a href="#">
+    <h5 className="mb-2 text-left text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Task Name: {name}</h5>
+</a>
+<p className="text-left mb-3 font-normal text-gray-700 dark:text-gray-400">Task Description: {description}</p>
+<div className='flex justify-between mb-3'>
+<Button onClick={handleMarkAsIncomplete}
+data-modal-toggle="authentication-modal" type="button"
+      className='text-left text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-2 text-center mr-2 mb-2 '  
+    >
+      Mark As Incomplete
+    </Button>
+<Button onClick={handleDelete}
+className='text-left  text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-2  text-center mr-2 mb-2'
+>
+Delete
+</Button>
+</div>
+</div>
+
+</div>
+        </div>
+    );
+};
+
+export default CompletedTask;
--- src/components/CompletedTasks/CompletedTasks.js
+++ src/components/CompletedTasks/CompletedTasks.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
 import { Helmet } from 'react-helmet';
+import { AuthContext } from '../../context/UserContext';
+import Loader from '../../Default/Loader/Loader';
+import CompletedTask from './CompletedTask';
 
 const CompletedTasks = () => {
+
+    const {user} = useContext(AuthContext);
+
+    const { data: completedtasks = [], isLoading} = useQuery({
+        queryKey: ['completedtasks'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/completedtasks?email=${user.email}`);
+            const data = await res.json();
+            return data
+        }
+    })
+
+    if(isLoading){
+        return <Loader/>
+    }
+
+
+
+
     return (
         <div>
               <Helmet>
@@ -9,6 +32,12 @@ const CompletedTasks = () => {
                     Completed Tasks
                 </title>
             </Helmet>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:ml-8'>
+            {
+                
+                completedtasks.map(completedtask =>  <CompletedTask completedtask={completedtask}></CompletedTask>)
+            }
+            </div>
         </div>
     );
 };
